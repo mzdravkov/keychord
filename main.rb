271710a3f06616e6fe2082de1033d175d86093c0
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2014-01-18 17:03:52 +0200

    Now works with qwerty keyboards

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require 'green_shoes'
 
+# in dvorak
 home_keys = %w[a o e u h t n s]
+# in qwerty
+qwerty_home_keys = %w[a s d f j k l ;]
 
 # optimized combinations
 #[["u", "e"],
@@ -32,8 +35,10 @@ home_keys = %w[a o e u h t n s]
 # ["t", "s"],
 # ["n", "s"]]
 
+keys = {}
+
 # letters that are not on the home keys
-keys = {
+keys['dvorak'] = {
   ['u', 'e'] => 'i',
   ['u', 'h'] => 'r',
   ['u', 't'] => 'd',
@@ -51,37 +56,94 @@ keys = {
   ['e', 's'] => 'j',
   ['e', 'a'] => 'x',
   ['o', 's'] => 'q',
-  ['a', 'h'] => 'z',
+  ['a', 'h'] => 'z'
+}
 
+keys['qwerty'] = {
+  'a' => 'a',
+  's' => 'o',
+  'd' => 'e',
+  'f' => 'u',
+  'j' => 'h',
+  'k' => 't',
+  'l' => 'n',
+  ';' => 's',
+  ['f', 'd'] => 'i',
+  ['f', 'j'] => 'r',
+  ['f', 'k'] => 'd',
+  ['f', 's'] => 'l',
+  ['f', 'l'] => 'c',
+  ['d', 'j'] => 'm',
+  ['d', 's'] => 'w',
+  ['d', 'k'] => 'f',
+  ['d', 'l'] => 'g',
+  ['s', 'j'] => 'y',
+  ['s', 'k'] => 'p',
+  ['s', 'l'] => 'b',
+  ['f', ';'] => 'v',
+  ['f', 'a'] => 'k',
+  ['d', ';'] => 'j',
+  ['d', 'a'] => 'x',
+  ['s', ';'] => 'q',
+  ['a', 'j'] => 'z'
 }
 
 
-complex_letters = keys.sort_by { |k, v| v }
+
+layout = 'dvorak'
+complex_letters = keys[layout].sort_by { |k, v| v }
 
 last_key = nil
 last_time = nil
 textstr = ""
 Shoes.app do
- e = edit_line
- info = para "NO KEY is PRESSED."
- text = para textstr
- info_complex_keys = para "Complex keys: \n"
- complex_letters.each do |k, v|
-   info_complex_keys.text = info_complex_keys.text + "#{v} -> #{k[0]} + #{k[1]}\n"
- end
- keypress do |k|
-   if last_key and Time.now - last_time < 0.05 and (keys[[last_key, k]] or keys[[k, last_key]])
-     k = (keys[[last_key, k]] or keys[[k, last_key]])
-     text.replace (textstr = textstr[0...-1])
-   end
-   info.replace "#{k} was PRESSED."
-   new_text = if k == 'BackSpace'
-     textstr = textstr[0...-1]
-   else
-     textstr += k
-   end
-   text.replace new_text
-   last_key = k
-   last_time = Time.now
- end
+  e = edit_line
+  layout_info = para layout
+  info_complex_keys = para ""
+  button 'Change layout' do
+    info_complex_keys.replace "Complex keys: \n result - input\n"
+    if layout == 'dvorak'
+      layout_info.replace (layout = 'qwerty')
+      complex_letters = keys['qwerty'].sort_by { |k, v| v }
+      qwerty_home_keys.each do |k|
+        info_complex_keys.text = info_complex_keys.text + "     #{keys['qwerty'][k]} - #{k}\n"
+      end
+      complex_letters.each do |k, v|
+        if k.is_a? Array
+          info_complex_keys.text = info_complex_keys.text + "     #{v} - #{k[0]} + #{k[1]}\n"
+        end
+      end
+    else
+      layout_info.replace (layout = 'dvorak')
+      complex_letters = keys['dvorak'].sort_by { |k, v| v }
+      complex_letters.each do |k, v|
+        info_complex_keys.text = info_complex_keys.text + "     #{v} - #{k[0]} + #{k[1]}\n"
+      end
+    end
+  end
+  info = para "NO KEY is PRESSED."
+  text = para textstr
+  info_complex_keys = para "Complex keys: \n result - input\n"
+
+  complex_letters.each do |k, v|
+    info_complex_keys.text = info_complex_keys.text + "     #{v} - #{k[0]} + #{k[1]}\n"
+  end
+  keypress do |k|
+    if layout == 'qwerty' and qwerty_key_index = qwerty_home_keys.index(k)
+      k = home_keys[qwerty_key_index]
+    end
+    if last_key and Time.now - last_time < 0.05 and (keys['dvorak'][[last_key, k]] or keys['dvorak'][[k, last_key]])
+      k = (keys['dvorak'][[last_key, k]] or keys['dvorak'][[k, last_key]])
+      text.replace (textstr = textstr[0...-1])
+    end
+    info.replace "#{k} was PRESSED."
+    new_text = if k == 'BackSpace'
+      textstr = textstr[0...-1]
+    else
+      textstr += k
+    end
+    text.replace new_text
+    last_key = k
+    last_time = Time.now
+  end
 end
